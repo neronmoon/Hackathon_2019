bba963a722877259d5166abf82894a8f220e9e25
neronmoon <alistar.neron@gmail.com>
2018-11-29 15:16:14 +0200

    Big Bang

---
+++ Assets/Sources/CameraFollower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    public Transform Target;
+
+    void Update () {
+        transform.position = Vector3.Lerp (transform.position, Target.position, 0.3f);
+    }
+}
---
+++ Assets/Sources/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Velocity = 200f;
+
+    void FixedUpdate () {
+        Rigidbody rb = GetComponent<Rigidbody> ();
+        if (Input.GetMouseButton (0)) {
+            Vector3 direction = getMovementDirection ();
+            rb.velocity = direction * Velocity * Time.deltaTime;
+            Quaternion delta = Quaternion.LookRotation (direction);
+            transform.rotation = Quaternion.Slerp (transform.rotation, delta, 0.3f);
+        } else {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private Vector3 getMovementDirection () {
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast (ray, out hit)) {
+            Vector3 point = hit.point;
+            point.y = transform.position.y;
+            return (point - transform.position).normalized;
+        }
+
+        return Vector3.zero;
+    }
+}
